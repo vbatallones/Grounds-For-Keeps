d55b511d4fbdbdf47dd215780d08088b75aba997
Levin <vbatallones.xvi@gmail.com>
2021-02-27 22:37:04 -0800

    install the ejs-mate and require it the index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override')
 
 const mongoose = require('mongoose');
@@ -18,11 +19,14 @@ db.once('open', () => {
     console.log("Database connected")
 })
 
+app.engine('ejs', ejsMate)
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
