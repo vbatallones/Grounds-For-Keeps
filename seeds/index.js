9adafe50a613eaed61ada270c146b10e32266545
Levin <vbatallones.xvi@gmail.com>
2021-03-01 15:54:39 -0800

    testing the image from our model and make a random price generator

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities')
-const {places, descriptors} = require('./seedHelpers')
+const { places, descriptors } = require('./seedHelpers')
 const Campground = require('../models/campground')
 
 mongoose.connect('mongodb://localhost:27017/ground-for-keeps', {
@@ -20,9 +20,13 @@ const seedDB = async () => {
     await Campground.deleteMany({})
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 20 + 10)
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore illo veniam explicabo eaque ex dolores nobis eligendi, doloremque mollitia quidem fugit, incidunt nemo, minus deserunt obcaecati repudiandae nulla debitis ab.",
+            price
         })
         await camp.save()
     }
