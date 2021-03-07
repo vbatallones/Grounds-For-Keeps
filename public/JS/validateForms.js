82bdd8f4ed122a1e17e2fd0118410a5c985db322
Levin <vbatallones.xvi@gmail.com>
2021-03-07 11:37:22 -0800

    create public folder that will include the JS, STYLE, and ASSETS. create a js folder that has a validationForm function pass in

---
+++ public/JS/validateForms.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict'
+    const forms = document.querySelectorAll('.validationForm')
+
+    // Loop over them and prevent submission
+    Array.from(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
