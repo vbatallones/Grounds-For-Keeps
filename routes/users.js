8cc591dca5e6ec8e46d9b59784f06c0c9e3b712f
Levin <vbatallones.xvi@gmail.com>
2021-03-10 00:41:04 -0800

    add login, register, logout routes

---
+++ routes/users.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const catchAsync = require('../error/catchAsync');
+const passport = require('passport');
+
+router.get('/register', (req, res) => {
+    res.render('users/register')
+})
+
+router.post('/register', catchAsync(async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if (err) return next();
+            req.flash('success', 'Welcome to CozY');
+            res.redirect('/campgrounds')
+        });
+
+    } catch (e) {
+        req.flash('error', e.message)
+        res.redirect('/register')
+    }
+}))
+
+router.get('/login', (req, res) => {
+    res.render('users/login')
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), async (req, res) => {
+    req.flash('success', 'Welcome back!')
+    res.redirect('/campgrounds')
+})
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', "Logged out")
+    res.redirect('/campgrounds')
+})
+
+module.exports = router
