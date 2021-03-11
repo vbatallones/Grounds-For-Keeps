35412a9e40521491f21cb43d84d2962430da0681
Levin <vbatallones.xvi@gmail.com>
2021-03-10 22:27:52 -0800

    add the isOwner function to the routes so that it wont be edited and deleted

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,22 +3,12 @@ const router = express.Router();
 const catchAsync = require('../error/catchAsync');
 const ErrorHandling = require('../error/ErrorHandling');
 const { campgroundSchema } = require('../schemaJoi.js');
-const {isLoggedIn} = require('../middleware')
+const { isLoggedIn, isOwner, validateForm } = require('../middleware')
 
 
 // MODELS
 const Campground = require('../models/campground');
 
-
-const validateForm = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ErrorHandling(msg, 400)
-    } else {
-        next();
-    }
-}
 
 // get all camp grounds
 router.get('/', catchAsync(async (req, res) => {
@@ -34,6 +24,7 @@ router.get('/new', isLoggedIn, catchAsync(async (req, res) => {
 // route to create a new campground
 router.post('/', isLoggedIn, validateForm, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
+    campground.owner = req.user._id;
     await campground.save();
     req.flash('success', 'Successfully made a new campground!')
     res.redirect(`/campgrounds/${campground._id}`)
@@ -42,7 +33,7 @@ router.post('/', isLoggedIn, validateForm, catchAsync(async (req, res, next) => 
 // show one camp ground by id
 router.get('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campgrounds = await Campground.findById(id).populate('reviews')
+    const campgrounds = await Campground.findById(id).populate('reviews').populate('owner')
     if (!campgrounds) {
         req.flash('error', 'Campground missing!');
         res.redirect('/campgrounds')
@@ -52,7 +43,7 @@ router.get('/:id', catchAsync(async (req, res) => {
 
 
 // render the edit form
-router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, isOwner, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id)
     if (!campground) {
@@ -63,7 +54,7 @@ router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
 }))
 
 // update the campground
-router.put('/:id', isLoggedIn, validateForm, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, isOwner, validateForm, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campgrounds = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { runValidators: true, new: true });
     req.flash('success', 'Updated Campground');
@@ -71,7 +62,7 @@ router.put('/:id', isLoggedIn, validateForm, catchAsync(async (req, res) => {
 }))
 
 // delete the campground
-router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, isOwner, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campgrounds = await Campground.findByIdAndDelete(id);
     req.flash('success', 'Deleted Campground');
