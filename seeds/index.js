53ea539a60ae66305fa438bc32315f8d89486cff
Levin <vbatallones.xvi@gmail.com>
2021-03-10 22:29:32 -0800

    add the owner to our model

--- seeds/index.js
+++ seeds/index.js
@@ -22,6 +22,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20 + 10)
         const camp = new Campground({
+            owner: "60486d0ffe69fe5f39bfabce",
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: "https://source.unsplash.com/collection/483251",
