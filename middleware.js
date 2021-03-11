b5b8740e0198750fce3f8330fe7b743146baedf7
Levin <vbatallones.xvi@gmail.com>
2021-03-10 22:26:09 -0800

    move the validateForm function to my middleware and create a isOwner function to check if the user had the same ID from the user that is logging in to authenticate if that user is the owner submits a campground

--- middleware.js
+++ middleware.js
@@ -1,8 +1,30 @@
 module.exports.isLoggedIn = (req, res, next) => {
-    console.log('REQ.USER.....', req.user)
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl
         req.flash('error', 'You must be signed');
         return res.redirect('/login')
     } 
     next();
+}
+
+
+module.exports.validateForm = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ErrorHandling(msg, 400)
+    } else {
+        next();
+    }
+}
+
+module.exports.isOwner = async (req, res, next) => {
+    const { id } = req.params;
+    const campgrounds = await Campground.findById(id);
+    if (!campgrounds.owner.equals(req.user._id)) {
+        req.flash('error', 'You do not have the permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    } else {
+        next();
+    }
 }
